4e3fabc0552677893b4b1bb212f511cfceca2eb8
Prashant Chaudhary <104970128+prashant369-designer@users.noreply.github.com>
2026-06-03 11:30:45 +0530

    first stage correction

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HeroBanner from "../Components/HeroBanner";
-import WeOffer from "../components/WeOffer";
+import WeOffer from "../Components/WeOffer";
 import AboutUs from "../components/AboutUs";
 import Testimonial from "../components/Testimonial";
 import WelcomeGym from "../components/WelcomeGym";
